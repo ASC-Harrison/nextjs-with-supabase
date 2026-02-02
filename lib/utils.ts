bf1efcac472cb3374e32b844545ba9b4df7fb67b
ASC-Harrison <hogstud800@gmail.com>
2026-02-02 12:16:31 -0600

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Tailwind class name helper used by shadcn/ui components.
- * Usage: cn("p-2", isActive && "bg-red-500")
- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-
