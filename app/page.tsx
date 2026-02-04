efec13716eff687191cfbfcaeece9644771b4ecd
ASC-Harrison <hogstud800@gmail.com>
2026-02-03 20:02:31 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,6 @@ export default function Page() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cameraOpen]);
 
-  function stopCamera() {
     if (scanLoopRef.current) {
       window.clearInterval(scanLoopRef.current);
       scanLoopRef.current = null;
