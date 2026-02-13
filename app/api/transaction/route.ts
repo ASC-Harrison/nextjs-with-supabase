497837db126ac3522a2ad280e2f1b80722a52bbb
ASC-Harrison <hogstud800@gmail.com>
2026-02-12 20:30:05 -0600

    Update route.ts

--- app/api/transaction/route.ts
+++ app/api/transaction/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-export const runtime = "nodejs";
+
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
