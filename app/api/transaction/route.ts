6d2f22ad11f457f94555b12d148f9482af63b030
ASC-Harrison <hogstud800@gmail.com>
2026-02-15 20:00:20 -0600

    Update route.ts

--- app/api/transaction/route.ts
+++ app/api/transaction/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-export const dynamic = "force-dynamic";
+
 
 type Body = {
   itemQuery: string; // name or barcode
