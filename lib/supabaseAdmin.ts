f7ad574964fe0e86120174929cdf9c2b49fc00d2
ASC-Harrison <hogstud800@gmail.com>
2026-02-19 21:10:39 -0600

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -3,7 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-// Server-only client (do NOT put service key in NEXT_PUBLIC)
 export const supabaseAdmin = createClient(url, serviceKey, {
   auth: { persistSession: false },
 });
