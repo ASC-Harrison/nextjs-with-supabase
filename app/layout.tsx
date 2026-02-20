2a347a1521ca894b19db1dc03024da590f6b9961
ASC-Harrison <hogstud800@gmail.com>
2026-02-20 09:57:00 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,10 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="h-full">
+      <body className="h-full bg-black text-white antialiased">
+        {children}
+      </body>
     </html>
   );
 }
