455104539a4b931515d3c515029afd9a8886fb83
ASC-Harrison <hogstud800@gmail.com>
2026-08-16 18:09:46 -0500

    Add finance loading screen

---
+++ app/finance/loading.tsx
@@ -0,0 +1,1 @@
+export default function Loading(){return <div style={{minHeight:'100vh',background:'#080d18',color:'#f3f7ff',padding:24,fontFamily:'system-ui'}}>Loading Financial Control Center…</div>}
